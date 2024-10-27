e1da874a7c85f397e4dfb72e9feeee1b89264b1e
zysuper <zysuper@gmail.com>
2024-10-27 21:34:24 +0800

    homework ch4 answer 2

---
+++ homework.md
@@ -0,0 +1,17 @@
+## 作业要求
+
+1. 与 ChatGPT 深度对话，尝试使用 System role 提升报告质量和稳定性。
+
+1. 练习 Gradio 基础组件和功能，尝试修改 v0.4.1 的布局。
+
+## 作业内容
+
+### Q1
+
+* [改进的提示词跳转 ./prompts/report_prompt.txt](./prompts/report_prompt.txt)
+* [改进的效果展示跳转](example/2024-10-20_to_2024-10-27_report.md)
+
+### Q2
+
+* [修改后的布局跳转](./src/gradio_server.py)
+![](./images/q2_1.png)
--- src/gradio_server.py
+++ src/gradio_server.py
@@ -21,10 +21,52 @@ def export_progress_by_date_range(repo, days):
 
     return report, report_file_path  # 返回报告内容和报告文件路径
 
-# 创建Gradio界面
-demo = gr.Interface(
+def add_subscription(repo):
+    if repo is None or repo == "":
+        return f"请输入要订阅的仓库名称"
+    # 定义一个函数，用于添加订阅
+    subscription_manager.add_subscription(repo)  # 调用订阅管理器的添加订阅方法
+    return f"已订阅 {repo}"  # 返回订阅成功的提示信息
+
+def remove_subscription(repo):
+    # 定义一个函数，用于删除订阅
+    subscription_manager.remove_subscription(repo)  # 调用订阅管理器的删除订阅方法
+    return f"已取消订阅 {repo}"  # 返回取消订阅成功的提示信息
+
+def save_subscriptions():
+    # 定义一个函数，用于保存订阅
+    subscription_manager.save_subscriptions()  # 调用订阅管理器的保存订阅方法
+    return "订阅已保存"  # 返回保存成功的提示信息
+
+def update_dropdown_options():
+    return gr.Dropdown(subscription_manager.subscriptions, label="选择要删除项目", choices=0)
+
+#订阅维护页面
+with gr.Blocks() as subscribe:
+    with gr.Row():
+        with gr.Column():    
+            # 输入框
+            output_text = gr.Textbox(label="执行结果", interactive=False)
+        with gr.Column():
+            # 输入框
+            input_text = gr.Textbox(label="新增开源项目（onwer/repo）")
+                
+            # 选择删除项目
+            choose_list = gr.Dropdown(subscription_manager.subscriptions, label="选择要删除项目"),
+
+            with gr.Row():
+                button_one = gr.Button("新增")
+                button_one.click(fn=add_subscription, inputs=input_text, outputs=output_text)
+                    
+                button_two = gr.Button("删除")
+                button_two.click(fn=remove_subscription, inputs=choose_list, outputs=output_text)
+                
+                output_text.change(fn=update_dropdown_options, outputs=choose_list)
+
+#创建Gradio界面
+report = gr.Interface(
     fn=export_progress_by_date_range,  # 指定界面调用的函数
-    title="GitHubSentinel",  # 设置界面标题
+    #title="GitHubSentinel",  # 设置界面标题
     inputs=[
         gr.Dropdown(
             subscription_manager.list_subscriptions(), label="订阅列表", info="已订阅GitHub项目"
@@ -35,7 +77,9 @@ demo = gr.Interface(
     outputs=[gr.Markdown(), gr.File(label="下载报告")],  # 输出格式：Markdown文本和文件下载
 )
 
+demo = gr.TabbedInterface([subscribe, report],["订阅维护", "每日报告"], title="GitHubSentinel")
+
 if __name__ == "__main__":
-    demo.launch(share=True, server_name="0.0.0.0")  # 启动界面并设置为公共可访问
+    demo.launch(share=True, server_name="0.0.0.0")  
     # 可选带有用户认证的启动方式
     # demo.launch(share=True, server_name="0.0.0.0", auth=("django", "1234"))
